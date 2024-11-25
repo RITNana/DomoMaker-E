d0b10f1f3c349032d1f70ceb4e29ae3705956a90
Nana Sarpong <ns8363@g.rit.edu>
2024-11-24 23:03:00 -0500

    DomoMaker-E

---
+++ client/maker.jsx
@@ -0,0 +1,110 @@
+const helper = require('./helper.js');
+const React = require('react');
+const { useState, useEffect } = React;
+const { createRoot } = require('react-dom/client');
+
+const handleDomo = (e, onDomoAdded) => {
+    e.preventDefault();
+    helper.hideError();
+
+    const name = e.target.querySelector('#domoName').value;
+    const age = e.target.querySelector('#domoAge').value;
+    const level = e.target.querySelector("#domoLevel").value
+
+    if (!name || !age || !level) {
+        helper.handleError('All fields are required');
+        return false;
+    }
+
+    helper.sendPost(e.target.action, { name, age, level }, onDomoAdded);
+    return false;
+}
+
+// create our FSC to create our Add Domo form
+const DomoForm = (props) => {
+    return (
+        <form id="domoForm"
+            onSubmit={(e) => handleDomo(e, props.triggerReload)}
+            name="domoForm"
+            action="/maker"
+            method="POST"
+            className="domoForm"
+        >
+            <label htmlFor="name">Name: </label>
+            <input id="domoName" type="text" name="name" placeholder="Domo Name" />
+            <label htmlFor="age">Age: </label>
+            <input id="domoAge" type="number" min="0" name="age" />
+            <label htmlFor="level">Level: </label>
+            <input id="domoLevel" type="number" min="1" name="level" />
+            <input className="makeDomoSubmit" type="submit" value="Make Domo" />
+        </form>
+    );
+};
+
+// create a components that will display our list of Domos
+/**
+ * We store domos array in useState to update and render list
+ * useEffect loads domos from the server and updates state
+ */
+const DomoList = (props) => {
+    const [domos, setDomos] = useState(props.domos);
+
+    useEffect(() => {
+        const loadDomosFromServer = async () => {
+            const response = await fetch('/getDomos');
+            const data = await response.json();
+            setDomos(data.domos);
+        };
+        loadDomosFromServer();
+    }, [props.reloadDomos])
+
+    if (domos.length === 0) {
+        return (
+            <div className="domoList">
+                <h3 className="emptyDomo">No Domos Yet!</h3>
+            </div>
+        );
+    }
+
+    const domoNodes = domos.map(domo => {
+        return (
+            <div key={domo.id} className="domo">
+                <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
+                <h3 className="domoName">Name: {domo.name}</h3>
+                <h3 className="domoAge">Age: {domo.age}</h3>
+                <h3 className="domoLevel">Level: {domo.level}</h3>
+            </div>
+        );
+    });
+
+    return (
+        <div className="domoList">
+            {domoNodes}
+        </div>
+    );
+};
+
+// function for domolist effect rerender 
+const App = () => {
+    const [reloadDomos, setReloadDomos] = useState(false);
+
+    return (
+        <div>
+            <div id="makeDomo">
+                <DomoForm triggerReload={() => setReloadDomos(!reloadDomos)} />
+            </div>
+            <div id="domos">
+                <DomoList domos={[]} reloadDomos={reloadDomos} />
+            </div>
+        </div>
+
+    );
+};
+
+const init = () => {
+    const root = createRoot(document.getElementById('app'));
+    root.render( <App />)
+};
+
+window.onload = init;
+
---
+++ server/router.js
@@ -0,0 +1,23 @@
+const controllers = require('./controllers');
+const mid = require('./middleware');
+
+// connect our routes to the page's configuration
+const router = (app) => {
+  app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
+
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
+
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
+
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
+
+  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
+
+  app.get('/about', mid.requiresSecure, controllers.Account.aboutPage);
+
+  app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+};
+
+module.exports = router;
